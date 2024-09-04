0155fcfe9dcc27327848532b8d57daa643509d19
SSAFY_NOTEBOOK <chae0738@naver.com>
2024-09-04 10:07:29 +0900

    Add: 초기세팅

---
+++ Backend/sliverorder/src/main/java/com/second/sliverorder/SliverorderApplication.java
@@ -0,0 +1,13 @@
+package com.second.sliverorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SliverorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SliverorderApplication.class, args);
+	}
+
+}
---
+++ Backend/sliverorder/src/test/java/com/second/sliverorder/SliverorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.second.sliverorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SliverorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
