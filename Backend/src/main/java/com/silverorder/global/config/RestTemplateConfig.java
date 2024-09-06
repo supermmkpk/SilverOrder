4021d4d3da041224612c5c299665352660a52d4a
채기훈 <chae0738@naver.com>
2024-09-06 12:07:15 +0900

    Add: 은행 API 생성 #S11P21C202-75

--- Backend/src/main/java/com/silverorder/global/config/RestTemplateConfig.java
+++ Backend/src/main/java/com/silverorder/global/config/RestTemplateConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 외부 API 요청 RestTemplate 설정
+ * @author 채기훈
+ * @since JDK17 Eclipse Temurin
+ */
 @Configuration
 public class RestTemplateConfig {
 
