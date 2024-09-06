fb5794352f9e2bfc18fc0fe04300aa140d0f7ab0
박동민 <pmin1616@naver.com>
2024-09-06 14:00:57 +0900

    Design: 매장 페이지 제목 및 음성인식 버튼 추가 #S11P21C202-27
    Former-commit-id: ce828e4ba97c532dbb60fda0491f815ade0e6f46

--- Frontend/Customer_app/silverorder/src/views/StorePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/StorePage.jsx
@@ -3,8 +3,12 @@ import "../styles/StorePage.css";
 const StorePage = () => {
   return (
     <div className="main-container">
-      <h1>안녕하세요 SilverOrder입니다.</h1>
-      <h2>살려줘 벌써 피곤해</h2>
+      <div className="store-title">
+        <h1>M커피 SSAFY점</h1>
+      </div>
+      <div className="SST-search-btn">
+        <img className="SST-search-img" src="/mike.png" alt="음성인식 버튼" />
+      </div>
       <div className="colorbox"></div>
     </div>
   );
