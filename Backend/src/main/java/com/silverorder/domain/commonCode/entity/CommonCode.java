202ca4af050db9ff523af9fe131e88a17bd477ab
노명환 <aud2531@naver.com>
2024-09-06 10:42:49 +0900

    Add : commonCode entity, dump data 추가
    Former-commit-id: 882e769d3977708bd91a75a0b8296ac7b253c2e6

---
+++ Backend/src/main/java/com/silverorder/domain/commonCode/entity/CommonCode.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f20f2df8457c89520bea78a7caeeb4afeabed17b2ac872ee3876076d4945281c
+size 782
---
+++ DATA/silverorder_commonCode_dump.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb0236652549a52ecb0fcdee7ad73d966167ad533e89fba5094ab68259ae6d39
+size 821
