af25ab3850ef43b7e5ec4fbc2368ff71ce5029e0
채기훈 <chae0738@naver.com>
2024-09-09 12:13:56 +0900

    Add:RabbitMQ 기본세팅 #S11P21C202-59

---
+++ Backend/src/main/java/com/silverorder/domain/order/service/OrderStatusProducer.java
@@ -0,0 +1,23 @@
+package com.silverorder.domain.order.service;
+
+import com.silverorder.global.config.RabbitMQConfig;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderStatusProducer {
+
+    private final AmqpTemplate amqpTemplate;
+
+    @Autowired
+    public OrderStatusProducer(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    public void sendOrderStatusUpdate(OrderStatusProducer event){
+        amqpTemplate.convertAndSend(RabbitMQConfig.ORDER_EXCHANGE,
+                RabbitMQConfig.ORDER_ROUTING_KEY, event);
+    }
+
+}
---
+++ Backend/src/main/java/com/silverorder/global/config/RabbitMQConfig.java
@@ -0,0 +1,47 @@
+package com.silverorder.global.config;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_EXCHANGE = "order-exchange";
+    public static final String ORDER_QUEUE = "order-queue";
+    public static final String ORDER_ROUTING_KEY = "order.status.changed";
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(ORDER_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, TopicExchange orderExchange) {
+        return BindingBuilder.bind(orderQueue).to(orderExchange).with(ORDER_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
