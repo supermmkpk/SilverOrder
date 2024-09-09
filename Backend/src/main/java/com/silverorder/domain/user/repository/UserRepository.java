8aac52ea69ccac2bc4311101041666b82f6b1c0f
박봉균 <supermmkpk@gmail.com>
2024-09-09 12:12:52 +0900

    Refactor: 관리자 회원가입 가맹점 번호 추가 #S11P21C202-76
    Former-commit-id: e79208d7630eaced3728706953b12a5337649398

--- Backend/src/main/java/com/silverorder/domain/user/repository/UserRepository.java
+++ Backend/src/main/java/com/silverorder/domain/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:073d3db8c67d0422c9425489ea6789cfe31c4def9b0aa6075e7bbf80468240a5
-size 627
+oid sha256:e6071dcb4fa06205fd7b116f3ff1dd8bcb97106778f09c2ec72d28d0988ae05d
+size 711
--- Backend/src/main/java/com/silverorder/domain/user/service/UserServiceImpl.java
+++ Backend/src/main/java/com/silverorder/domain/user/service/UserServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f21a9ab4b6937ebc7fd2dfb445119578d162482ee7630e3713a57a442789b875
-size 3460
+oid sha256:13d937e659b8c363bc976cdf6c1bd9f2fe52ad558cff007514493a53b0b097f7
+size 4240
