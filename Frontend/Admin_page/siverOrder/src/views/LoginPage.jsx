7bbe31a434d6ad85456387298fa5881b4d01908e
CheonYoSeong <gwpeter1000@naver.com>
2024-09-09 13:47:09 +0900

    Add :useInfoStore #S11P21C202-81

--- Frontend/Admin_page/siverOrder/src/views/LoginPage.jsx
+++ Frontend/Admin_page/siverOrder/src/views/LoginPage.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import useInfoStore from "../../stores/infos";
 import '../styles/LoginPage.css';
 
 const LoginPage = () => {
+  const { sendLoginRequest } = useInfoStore();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const success = await sendLoginRequest(email, password);
+    if (success) {
+      Notiflix.Notify.success('로그인 성공');
+    } else {
+      Notiflix.Notify.failure('비밀번호가 일치하지 않습니다.');
+    }
+  };
+
   return (
     <div className="login-container">
+      <form onSubmit={handleSubmit}>
       <img src="/src/logo/logo.png" alt="Logo" className="login-image" />
       <input
         type="text"
@@ -19,6 +36,8 @@ const LoginPage = () => {
       <div className="signup-text">
         회원이 아니신가요? <a href="/signup" className="signup-link">회원 가입</a>
       </div>
+      </form>
+      
     </div>
   );
 };
--- Frontend/Admin_page/siverOrder/src/views/Signup.jsx
+++ Frontend/Admin_page/siverOrder/src/views/Signup.jsx
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Signup.css';
+import useInfoStore from "../../stores/infos";
 
 const Signup = () => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const { sendLoginRequest, sendRegisterRequest } = useInfoStore();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [birth, setBirth] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [isPasswordMismatch, setIsPasswordMismatch] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   useEffect(() => {
     // 비밀번호와 비밀번호 재입력 값이 다르면 하이라이트 설정
     setIsPasswordMismatch(password !== confirmPassword && confirmPassword.length > 0);
   }, [password, confirmPassword]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // 비밀번호 확인 검증
+    if (password !== confirmPassword) {
+      Notiflix.Notify.failure('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    setError("");
+    const success = await sendRegisterRequest(email, password, birth);
+    if (success) {
+      sendLoginRequest(email, password);
+    }
+  };
 
   return (
     <div className="signup-container">
@@ -18,6 +40,8 @@ const Signup = () => {
         type="text"
         placeholder="아이디를 입력하세요"
         className="signup-input"
+        onChange={(e) => setEmail(e.target.value)}
+        required
       />
       <input
         type="password"
@@ -25,6 +49,7 @@ const Signup = () => {
         className="signup-input"
         value={password}
         onChange={(e) => setPassword(e.target.value)} // 비밀번호 입력 핸들러
+        required
       />
       <input
         type="password"
@@ -32,6 +57,7 @@ const Signup = () => {
         className={`signup-input ${isPasswordMismatch ? 'error' : ''}`} // 비밀번호 불일치 시 'error' 클래스 추가
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)} // 비밀번호 재입력 핸들러
+        required
       />
       <input
         type="text"
@@ -40,8 +66,11 @@ const Signup = () => {
       />
       <input
         type="text"
-        placeholder="우편 번호 찾기"
+        placeholder="생년월일 0000-00-00"
         className="signup-input"
+        value={birth}
+        onChange={(e)=>setBirth(e.target.value)}
+
       />
       <button className="signup-button">회원 가입</button>
     </div>
