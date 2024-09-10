b7352ff18ff5a80d20561141d28cb1f9b8c298d6
박동민 <pmin1616@naver.com>
2024-09-10 15:40:57 +0900

    Add, Feat: QR 없이 접속되는 페이지, 지도 페이지 #S11P21C202-33
    Former-commit-id: 1dc16fba6b064c61ba7f3808809c14322bb2d431

--- Frontend/Customer_app/silverorder/src/routes/PageRoutes.jsx
+++ Frontend/Customer_app/silverorder/src/routes/PageRoutes.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4cdf34f321a73ea3fb2eb31841f56f401e6d903983216bc98dff77a0b0636f6
-size 1703
+oid sha256:a0cea63cdcf2aa7d572c39705e07ea07a860b9e958f7ea46a003193f37c01b4b
+size 2547
