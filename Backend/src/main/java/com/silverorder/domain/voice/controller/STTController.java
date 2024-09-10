06b037722699ba40fb1903ecf8501f5d8c37b29e
박봉균 <supermmkpk@gmail.com>
2024-09-10 13:44:38 +0900

    Feat: CLOVA STT 연동 확인 #S11P21C202-86 #in-progress
    Former-commit-id: 1e42f5449e0cbbdb4c3abc1c73908ccc25f767ac

---
+++ Backend/src/main/java/com/silverorder/domain/voice/controller/STTController.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a825e69cdc1f64dd972ee557145d310e3d33ebcc21bb875b39fccc69302057dd
+size 1919
---
+++ Backend/src/main/java/com/silverorder/domain/voice/service/STTService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d84c2f2cd145e02a2b4ca3c4842c69272098fb7d4c3f0be93ad3bf14499c949
+size 2287
