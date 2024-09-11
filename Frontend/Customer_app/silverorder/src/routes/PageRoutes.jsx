4cd5f91be536ab0faa8e9e1a2c96ac387d9a8b04
박동민 <pmin1616@naver.com>
2024-09-11 11:13:53 +0900

    Feat: 마이페이지의 내 정보 수정 삭제, 메뉴 카테고리에 따른 필터링 버튼 추가 #S11P21C202-27
    Former-commit-id: 6ab68f136cb0ed051264c5f648d8de9ea7efe88e

--- Frontend/Customer_app/silverorder/src/routes/PageRoutes.jsx
+++ Frontend/Customer_app/silverorder/src/routes/PageRoutes.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0cea63cdcf2aa7d572c39705e07ea07a860b9e958f7ea46a003193f37c01b4b
-size 2547
+oid sha256:38395d853dab6ea9e499109f1891902d9a3f830263e64a6b2885ec2a4db43c2c
+size 2337
--- Frontend/Customer_app/silverorder/src/views/MyPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/MyPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20bdcecdf950cb3f467a582aa6e2e94db2fa63511579bd80d6544e977f35d2e9
-size 1000
+oid sha256:2bcd4e62ed41a2cb6e1aa309ecd0ccafbf76a5b5901c1cdd83a24e8b28bc5275
+size 754
