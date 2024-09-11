f6400d02aa397ff9d34bd29f505099155667fa1d
CheonYoSeong <gwpeter1000@naver.com>
2024-09-11 17:55:29 +0900

    Feat: 반응형 적용
    Former-commit-id: 9e2248631c5fafb583ee08b8235e2753ffcc7f3f

--- Frontend/Admin_page/siverOrder/src/views/MenuPage.jsx
+++ Frontend/Admin_page/siverOrder/src/views/MenuPage.jsx
@@ -1,13 +1,3 @@
-import React, { useState } from 'react';
-import Navbar from '../components/Navbar/Navbar';
-
-
-const MenuPage = () => {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    );
-}
-
-export default MenuPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:bd075696d9a97186428dd87865c651d71d78f2df5f6eada687a3543fd161cd78
+size 1650
--- Frontend/Admin_page/siverOrder/src/views/OrderPage.jsx
+++ Frontend/Admin_page/siverOrder/src/views/OrderPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87aa54c50a96c74dd5035467bd0a8ab33dcb08963a719b3ae3be33d9fd452f05
-size 1073
+oid sha256:9f62a9b1adad40d04de19b00c2352e8da326467027c753dacaca020c971fc2dd
+size 1334
