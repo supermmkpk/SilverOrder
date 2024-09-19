09735ddfb5cf15f989166913a299b80ad8a4c80d
박동민 <pmin1616@naver.com>
2024-09-19 14:27:39 +0900

    Feat: 메뉴 검색 기능 추가, info.js의 API 요청 url 수정 #S11P21C202-27
    Former-commit-id: 3c877c507b74755becddf4b7ad8b4677ef91527d

--- Frontend/Customer_app/silverorder/src/components/MenuCard/MenuList.jsx
+++ Frontend/Customer_app/silverorder/src/components/MenuCard/MenuList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e10392ef947c6ba21834c3a6500295f9fa5315f7d24964d3d91f9d91c5cf90e
-size 3444
+oid sha256:73bff67c01172aa8d0e7df9baf80d0b7dfc0b54c5ac938c02a38c2b422f5911c
+size 5076
--- Frontend/Customer_app/silverorder/src/stores/infos.js
+++ Frontend/Customer_app/silverorder/src/stores/infos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d280a5e27b99676529054aaed2839eea005594c7def604b0d40ef1fa27bcd6b7
-size 1810
+oid sha256:0d56b204cddef7303aab00f0ea224252c44c75e9378470471b0a86b0b40d8cae
+size 1849
