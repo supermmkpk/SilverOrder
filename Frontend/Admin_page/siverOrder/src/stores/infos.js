2c053a6af45e2499fe3b3495935451e92c30d175
CheonYoSeong <gwpeter1000@naver.com>
2024-09-19 14:19:42 +0900

    Feat : api 요청 주소 변경
    Former-commit-id: 73eb8b5574737d01668fff42913124cdabb08a42

--- Frontend/Admin_page/siverOrder/src/stores/infos.js
+++ Frontend/Admin_page/siverOrder/src/stores/infos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40ba7efec4e49b16eaba32f4e237a050ac6cc46ca9f27525437e22a1055b465f
-size 2549
+oid sha256:f43f163d42e7bf944d23c07c5b7b9e28edb1b1b1e7a6e27d5cbcb54f28c09fac
+size 2590
--- Frontend/Admin_page/siverOrder/src/stores/option.js
+++ Frontend/Admin_page/siverOrder/src/stores/option.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18a42a08daa9c0f53a432dcffb488e92e3c11ea8212df7dfd02b6b4a71852a78
-size 1962
+oid sha256:9e9816a40be7e4092f1764cf00c4aa59e7c9f973e7b8aa5861294e0b454f12f8
+size 2002
