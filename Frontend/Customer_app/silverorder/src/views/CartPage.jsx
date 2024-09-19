c78666952533c6a1115f8abbb0b9540f11d7ffcc
박동민 <pmin1616@naver.com>
2024-09-19 13:33:15 +0900

    Feat: 옵션선택 버튼 디자인 수정, 장바구니에서 선택한 옵션 없는 상황 고려하여 코드 수정 #S11P21C202-40
    Former-commit-id: a95e8aa7001970ccca0676d3834e474015f45b3a

--- Frontend/Customer_app/silverorder/src/views/CartPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/CartPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d085acaf7b7a60b24e8b53c409296711b214f3ab774ad2f73aafb4ff537bd599
-size 2852
+oid sha256:dbd8b369d54e4e46fbd5df292425a8b27b2a2b05ee713ef0fdcb0d65dbdcf3bb
+size 2881
