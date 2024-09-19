b3c42471cac45373cc14b261e3e8e07bc7406a23
박동민 <pmin1616@naver.com>
2024-09-19 15:42:53 +0900

    Design, Docs: 내 카드 페이지 작업중, 접속 주소 앞에 /silverorder 추가 #S11P21C202-18
    Former-commit-id: 2a630abd0cfb037920b8ec0061b9d409f1a5856a

--- Frontend/Customer_app/silverorder/src/components/Navbar/DownsideNavbar.jsx
+++ Frontend/Customer_app/silverorder/src/components/Navbar/DownsideNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57b990a0d640a604650f01e19be9c6a4affe260e6cb62338979f3a6bf7f36203
-size 1259
+oid sha256:985a679de79b98d69ed3e0dbe912abf729f15ded981430ed1a2e44c910e21001
+size 1307
