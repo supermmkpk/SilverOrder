d22f94853e23d5b25bebbf001c2887b011cf6cf9
박동민 <pmin1616@naver.com>
2024-09-20 11:23:11 +0900

    Feat: API를 활용하여 금융권 카드 조회하는 기능 추가 #S11P21C202-18
    Former-commit-id: e18ff991a6dceab93643db8932dd06731d712c8f

--- Frontend/Customer_app/silverorder/src/stores/purchase.js
+++ Frontend/Customer_app/silverorder/src/stores/purchase.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:165f59ce7d2de6476c7c2c853888ffd73f7529f65a8d299b856c842be32b5268
-size 1053
+oid sha256:b4f052a9523ef2fc5caa1e03c2d3975d177902c02de9d9b321b9cf3f93bcb0a1
+size 1422
--- Frontend/Customer_app/silverorder/src/views/RegisterCardPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/RegisterCardPage.jsx
@@ -1,11 +1,3 @@
-import "../styles/RegisterCardPage.css";
-
-const RegisterCardPage = () => {
-  return (
-    <div>
-      <h1>카드 등록하기</h1>
-    </div>
-  );
-};
-
-export default RegisterCardPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8ca5b74293d29614ce814de2d9fb16e59d807e363702043a1e43e527129dfc5
+size 3037
