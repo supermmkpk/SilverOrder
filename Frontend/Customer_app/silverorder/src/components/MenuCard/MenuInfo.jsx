e0cd772db3ef6c0471aa4f26d7e2b2ddfebb285c
박동민 <pmin1616@naver.com>
2024-09-20 11:55:51 +0900

    Design: 이미지를 사용하는 방법을 public에서 import를 활용한 직접할당으로 변경 #S11P21C202-44
    Former-commit-id: 0807bbea59ec46d2bd6ddc595957a4a59750c9d7

--- Frontend/Customer_app/silverorder/src/components/MenuCard/MenuInfo.jsx
+++ Frontend/Customer_app/silverorder/src/components/MenuCard/MenuInfo.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c721a42311d18a8b76cf9f98e103ceef384b2135b29021807e525c12308736e
-size 2536
+oid sha256:a9e87a6bf7d291b8be94c01c29eae73a84557ba18cc67f6214411ab405ee8795
+size 2812
--- Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
+++ Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4817cf3a4aa9f9637ae166cf6ab623482f7b62cb4228da10ab6ddbb6fcd0d649
-size 1177
+oid sha256:463213bc922ed46894c427a3b4236454631c0f645dea35c19bddf4dca8471731
+size 1309
