bc7be0129ba68ae38bf4cf5e061e842545354349
박동민 <pmin1616@naver.com>
2024-09-23 10:52:38 +0900

    Feat: 간편 결제 등록된 카드 조회 기능 추가, userApiEmail없을 시 카드 등록 페이지로 이동 못하게 방지 #S11P21C202-18
    Former-commit-id: 0b81589d3faeafa96c57744beea51408a1033d42

--- Frontend/Customer_app/silverorder/src/views/MycardPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/MycardPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4549f66f408a67cd35065694a0416dcd071979e02dbc9fb349e50266e33ccb9e
-size 1575
+oid sha256:a07eb63875827f36bcef218471cb13e821541691eee250d5071a5a2a83d3c482
+size 3115
