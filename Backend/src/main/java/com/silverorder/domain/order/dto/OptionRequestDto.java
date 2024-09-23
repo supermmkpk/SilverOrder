bef392984b4ae64bc514665867c12a669778d470
박봉균 <supermmkpk@gmail.com>
2024-09-23 12:10:13 +0900

    Feat: 주문 정보 저장 구현 완료 #S11P21C202-100 #done
    Former-commit-id: 50af344da959ebfec28998f2871ffadbf195e54b

--- Backend/src/main/java/com/silverorder/domain/order/dto/OptionRequestDto.java
+++ Backend/src/main/java/com/silverorder/domain/order/dto/OptionRequestDto.java
@@ -1,14 +1,3 @@
-package com.silverorder.domain.order.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class OptionRequestDto {
-    private Long optionId;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1590b2c0d1e86dd12eab6cb65da94973d4f9f05b589939f268e257ac8be5b666
+size 741
---
+++ Backend/src/main/java/com/silverorder/domain/order/dto/OrderDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e4e6f266330cf8dc25937c9dcacf88e263834ae7f9801934a61b89e983715bd0
+size 1074
