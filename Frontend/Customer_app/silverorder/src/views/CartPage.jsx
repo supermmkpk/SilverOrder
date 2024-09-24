e83290adc10d422df876e9e14e2f409a06090632
박동민 <pmin1616@naver.com>
2024-09-24 14:07:24 +0900

    Add, Feat: 결제 페이지 생성 & 카드 목록 불러오는 API와 결제 API 연동만 하면 동작 가능하도록 구현함 #S11P21C202-18
    Former-commit-id: f6d69317b75ee4b9c2444ff4a9762830be588157

--- Frontend/Customer_app/silverorder/src/views/CartPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/CartPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbd8b369d54e4e46fbd5df292425a8b27b2a2b05ee713ef0fdcb0d65dbdcf3bb
-size 2881
+oid sha256:a284da7393d79200ca168e6f3c37f11eedfc91f1c3556d4ab869a472c18a9245
+size 3624
---
+++ Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ca18d2596b69c1d88678bdc269ece1a02b4d19a1ddf2644b4a379a43c82fe1e
+size 5847
