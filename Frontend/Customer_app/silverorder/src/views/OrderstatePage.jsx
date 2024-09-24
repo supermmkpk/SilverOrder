33750402ceff5eb52072c6b5e2cadfb3c764e30c
박동민 <pmin1616@naver.com>
2024-09-24 17:32:13 +0900

    Feat: 더미데이터 활용하여 화면 디자인 & 필요한 데이터 관리 #S11P21C202-58
    Former-commit-id: fe4b73e1ea73743c26c5acbd670e0e654507eb05

--- Frontend/Customer_app/silverorder/src/views/OrderstatePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/OrderstatePage.jsx
@@ -1,11 +1,3 @@
-import "../styles/OrderstatePage.css";
-
-const OrderstatePage = () => {
-  return (
-    <div>
-      <p>주문 현황 확인 페이지</p>
-    </div>
-  );
-};
-
-export default OrderstatePage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:075e64eeba1b87be40f9582d58789547cbc1c51c7e13702e047d32f8c5f62d98
+size 3292
