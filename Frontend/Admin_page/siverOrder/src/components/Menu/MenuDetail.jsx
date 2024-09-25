2dc8fbe8ceac2c2962a12fe74ec2729731f208ff
CheonYoSeong <gwpeter1000@naver.com>
2024-09-25 17:06:07 +0900

    Feat : 리뷰 전체 조회 및 관리자 댓글 작성 기능 연결 #S11P21C202-111
    Former-commit-id: 0123503d1ec3e7268a78318d96511b1621a117f3

--- Frontend/Admin_page/siverOrder/src/components/Menu/MenuDetail.jsx
+++ Frontend/Admin_page/siverOrder/src/components/Menu/MenuDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c2084b20dce4ce9be7530f7d7c35be3086b9a40b1987f8cf8ccc19e64ed3f8b
-size 2392
+oid sha256:f098150a794994bc37b47e5ea0be5e9cc2078d968cd23ad1159aa1a4356aad35
+size 2229
