3055ba8b49ddfba4221fb4fd83e2c0f588066b99
박동민 <pmin1616@naver.com>
2024-09-25 17:18:04 +0900

    Add, Feat: 지난 주문 내역 페이지에서 간략한 주문 내역 확인, modal을 사용한 상세정보 확인 기능 구현 #S11P21C202-112
    Former-commit-id: 065d5a278aae394c11db7dbc6ab808ff6eed41c0

---
+++ Frontend/Customer_app/silverorder/src/components/OrderCard/MyOrders.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2b1e7096666e898b015b89fa333a70d8b59d95c2df558973c02c9da132b713da
+size 2049
---
+++ Frontend/Customer_app/silverorder/src/components/OrderCard/OrderDetailInfo.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff3eb0e73a25827d303b3f0d7e82048ab185bd28eae4f782769002eddefab6c0
+size 2370
