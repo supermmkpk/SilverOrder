6d766c0fb6702c439adb802fa1c6b76b6f31bde4
박동민 <pmin1616@naver.com>
2024-09-26 10:58:45 +0900

    Feat: 리뷰 작성할 수 있는 input 추가, 작성한 내용 확인 버튼 누르면 console로 확인 가능 #S11P21C202-38

--- Frontend/Customer_app/silverorder/src/components/OrderCard/CheckReview.jsx
+++ Frontend/Customer_app/silverorder/src/components/OrderCard/CheckReview.jsx
@@ -1,20 +1,30 @@
 import "./styles/CheckReview.css";
+import { useState } from "react";
 
 const CheckReview = ({ orderId, onClose }) => {
+  const [review, setReview] = useState("");
+
   const handleReviewResult = () => {
+    console.log(review);
     onClose();
   };
 
   return (
     <div className="review-overlay">
       <div className="review-container">
-        <p>리뷰 작성하기</p>
+        <p className="review-title">리뷰 작성하기</p>
+        <textarea
+          className="review-input"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+          placeholder="여기에 작성해주세요"
+        ></textarea>
         <div className="review-btns">
           <button className="review-write-btn" onClick={handleReviewResult}>
-            완료
+            완 료
           </button>
           <button className="review-cancel-btn" onClick={onClose}>
-            취소
+            취 소
           </button>
         </div>
       </div>
