1c0b26c5f2b834f62a3cd1eb00064fca081b130c
박동민 <pmin1616@naver.com>
2024-09-26 10:58:45 +0900

    Feat: 리뷰 작성할 수 있는 input 추가, 작성한 내용 확인 버튼 누르면 console로 확인 가능 #S11P21C202-38
    Former-commit-id: 6d766c0fb6702c439adb802fa1c6b76b6f31bde4

--- Frontend/Customer_app/silverorder/src/components/OrderCard/CheckReview.jsx
+++ Frontend/Customer_app/silverorder/src/components/OrderCard/CheckReview.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce637b3c732ed956897b9b0e723b60359affc15892fa5f52740f227a6731c57d
-size 602
+oid sha256:14e31671382e52219fc05eacbad23efc1727a5568570ad84787219ab8bd26dd3
+size 939
