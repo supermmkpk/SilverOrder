3b9ea29e9e55f54146b9c40e3d2a4927bea2c6e4
채기훈 <chae0738@naver.com>
2024-09-26 12:03:01 +0900

    Add: 위치기준 매장 조회 추가 #S11P21C202-89
    Former-commit-id: 1c33b9b4c3ecca75faa8ff75f631ac7387dc7a30

--- Backend/src/main/java/com/silverorder/domain/store/dto/ResponseLatitudeLongitudeDTO.java
+++ Backend/src/main/java/com/silverorder/domain/store/dto/ResponseLatitudeLongitudeDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7cebaede346353fad76752e4dd4c83d63232ada11b58414a22d8fa67a15a0ee
-size 567
+oid sha256:d2b7e53162dd152ebcae7eb46a3119eafc6ffeef16c0f3efa99cc8196fa545d0
+size 576
