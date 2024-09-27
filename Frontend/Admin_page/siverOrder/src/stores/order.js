7e74ca4218c08726269b7645cd0ff6eaa1608904
CheonYoSeong <gwpeter1000@naver.com>
2024-09-27 13:22:36 +0900

    Feat: 주문 조회 기능 연결 중 #S11P21C202-115
    Former-commit-id: c18726ef3cdd4deb089edd6700cefd4aa3d2bdee

--- Frontend/Admin_page/siverOrder/src/stores/order.js
+++ Frontend/Admin_page/siverOrder/src/stores/order.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3da496157ea50a02103b17e1b54f55dd8639d3b4cd84eb0c3bd507535d938a68
-size 1096
+oid sha256:cf18b4422777230cecae530d5cc697acf843029796bb5c833c8e0884e33ee13a
+size 2112
