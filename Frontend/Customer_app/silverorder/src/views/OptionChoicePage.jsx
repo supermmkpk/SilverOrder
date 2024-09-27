47cf24a5cde13b48d388a00a3caa1ce5f8392eff
박동민 <pmin1616@naver.com>
2024-09-27 10:32:20 +0900

    Feat: 결제 요청 API 적용하여 결제 기능 추가 성공 #S11P21C202-18
    Former-commit-id: 527133e83392dc6c13568a3fbb9c9b2bc567bda0

--- Frontend/Customer_app/silverorder/src/views/OptionChoicePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/OptionChoicePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e671f77a1c1e58e662f66c177871ce747697aa09f5b6e4fdb3323df2286b7200
-size 4540
+oid sha256:0ce0ff7fff4f23166719d9aa984e14393ad0b8e1fa7fe880740e74d4e6003297
+size 4639
--- Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e19a2781eccc9e5fc06ddfe531c1be62eb12aee0a8444af37aa05f04351c7eea
-size 6165
+oid sha256:da428eb55e849391d64a20a5d9695300c8e501a9213a36706c826c0eb3cb7f96
+size 7281
