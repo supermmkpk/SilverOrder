071793fa00af1cf817cb605879052bb17db41368
박봉균 <supermmkpk@gmail.com>
2024-09-27 10:43:02 +0900

    Refactor: Swagger에서 200(OK)_ExpectedValue 추가
    Former-commit-id: 08ea12cb8b3ce751d203d8d8857673ce6ba6341d

--- Backend/src/main/java/com/silverorder/domain/order/controller/OrderController.java
+++ Backend/src/main/java/com/silverorder/domain/order/controller/OrderController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35ffbe621106c6afc91851fdef4a7d7e6a67b3614bc4d884dc424e61622115dc
-size 4412
+oid sha256:ed21f93465163ca7d94d4fa7374c3d9962085e08e9d8746214c439d943351025
+size 6659
