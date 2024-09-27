9beb30055afc9315590f569400116a4862273045
노명환 <aud2531@naver.com>
2024-09-27 14:26:32 +0900

    Refactor : AUTH_WHITELIST 추가
    Former-commit-id: 927f90766a219ab69717146569975d2ef7769ffc

--- Backend/src/main/java/com/silverorder/global/config/security/SecurityConfig.java
+++ Backend/src/main/java/com/silverorder/global/config/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42147b7e9dc2d900f65fd82ed94f22cee49ae2ef2b76c8798c992278cb2f0631
-size 5331
+oid sha256:1d0503dac32b1c3f3f7225438e216005eb4d3742553fa18754d07caa4b66a25a
+size 5371
