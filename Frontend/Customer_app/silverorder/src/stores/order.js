26fec955719edf45f11581426b98add49d04dc53
박동민 <pmin1616@naver.com>
2024-09-27 15:04:04 +0900

    Feat: 지난 주문 내역, 주문내역 상세 API로 불러와 출력 #S11P21C202-112

--- Frontend/Customer_app/silverorder/src/stores/order.js
+++ Frontend/Customer_app/silverorder/src/stores/order.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 import useInfoStore from "./infos";
+import { baseURL } from "../constant";
 
 const API_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/silverorder/";
@@ -23,6 +24,43 @@ const useOrderStore = create(() => ({
       return response.data;
     } catch (error) {
       console.error("최근 주문 현황 로딩 실패:", error);
+    }
+  },
+
+  // 내 지난 주문 내역 확인하기
+  fetchBeforeOrderList: async () => {
+    const { token } = useInfoStore.getState();
+
+    try {
+      const response = await axios.get(`${API_URL}order/myOrder`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log("지난 주문 내역:", response.data);
+      return response.data;
+    } catch (error) {
+      console.log("지난 주문 내역 로딩 실패:", error);
+    }
+  },
+
+  // 주문 내역 상세 조회
+  fetchBeforeOrderDetail: async (orderId) => {
+    const { token } = useInfoStore.getState();
+
+    try {
+      const response = await axios.get(
+        `${API_URL}order/orderDetail/${orderId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("주문 내역 상세:", response.data.menuList);
+      return response.data.menuList;
+    } catch (error) {
+      console.error("주문 내역 상세 로딩 실패:", error);
     }
   },
 }));
