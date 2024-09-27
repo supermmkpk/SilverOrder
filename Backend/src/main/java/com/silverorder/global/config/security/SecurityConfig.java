e18efc7d8f0a426a68b4e100742bf47d9cf7f87b
노명환 <aud2531@naver.com>
2024-09-27 17:50:57 +0900

    Fix : 배포 cors 해결 시도 #9
    Former-commit-id: 7d2371c210a60c6fda195be28810241a429a7279

--- Backend/src/main/java/com/silverorder/global/config/security/SecurityConfig.java
+++ Backend/src/main/java/com/silverorder/global/config/security/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f6bad595da0bd0a17a354d62901bc4588d9263655c87cebb8f249aae164959e
-size 5653
+oid sha256:34a2385a82827215b92d019b28dfe2b15e16fd6688d51fbf1fe8cced2e49045a
+size 5983
