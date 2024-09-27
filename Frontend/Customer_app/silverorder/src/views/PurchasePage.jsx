3e6f4a044664404081cd232ccd855da82c953370
박동민 <pmin1616@naver.com>
2024-09-27 11:21:37 +0900

    Fix: 결제 API 전송 시 data 잘못 전달되는 에러 해결 #S11P21C202-18
    Former-commit-id: 4bfa39bd7ca4f7918a5a733f5d285624ce1535f5

--- Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da428eb55e849391d64a20a5d9695300c8e501a9213a36706c826c0eb3cb7f96
-size 7281
+oid sha256:41789003b359c7bf61d98c9d98dd54f60fc1d76f7ac9afb05f33c7827e99284f
+size 7339
