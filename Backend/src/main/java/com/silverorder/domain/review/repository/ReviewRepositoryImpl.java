226ffd95d0f73b2cd733faa10506c283cf2778fd
박봉균 <supermmkpk@gmail.com>
2024-09-30 14:08:32 +0900

    Refactor: 리뷰 조회 API 이미지 추가
    Former-commit-id: 20f65ff37fbf82721753c419235ff4d6ca85bae0

--- Backend/src/main/java/com/silverorder/domain/review/repository/ReviewRepositoryImpl.java
+++ Backend/src/main/java/com/silverorder/domain/review/repository/ReviewRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54fefa3993bd9f4c9fdb9032b3fb920521c4dc35d1275c216fa1a8aaed586cfc
-size 7467
+oid sha256:be341101dce5dfa040cdb599a18ea15f4e7b6776062252f3f63e4420e3116568
+size 7631
