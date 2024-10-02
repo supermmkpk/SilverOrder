33153a381ea8315af90abdea65bccd0fffc58fc0
CheonYoSeong <gwpeter1000@naver.com>
2024-10-02 17:44:34 +0900

    Add : 관리자 대시보드 연령별 인기상품 차트  #479447a8

--- Frontend/Admin_page/siverOrder/src/stores/websocket.js
+++ Frontend/Admin_page/siverOrder/src/stores/websocket.js
@@ -2,35 +2,29 @@ import { create } from 'zustand';
 import SockJS from 'sockjs-client';
 import { Stomp } from "@stomp/stompjs";
 import useOrderStore from './order'; // 주문 목록 상태 관리하는 스토어
-
 const useWebSocketStore = create((set, get) => ({
   stompClient: null,
 
   // WebSocket 연결
   connectWebSocket: (storeId) => {
-      // // 현재 페이지의 프로토콜이 https일 경우 https, 아니면 http를 사용
-      // const protocol = window.location.protocol === "https:" ? "https:" : "http:";
-      // console.log(window.location.protocol);
-      // // 현재 호스트를 기준으로 소켓 URL을 동적으로 설정
-      // const socket = new SockJS(
-      //     `${protocol}//${window.location.host}/silverorder/api/ws-stomp`
-      // );
-    
-      // console.log(
-      //     `${protocol}//${window.location.host}/silverorder/api/ws-stomp`
-      // );
-    
-      const client = Stomp.over(socket);
+    if (!storeId) {
+      console.error('storeId가 없습니다. WebSocket을 연결할 수 없습니다.');
+      return;
+    }
+
     // SockJS를 사용하여 서버에 연결
-    const socket = new SockJS('http://localhost:8080/silverorder/ws-stomp');
-    const stompClient = Stomp.over(socket);
+    const socket = new SockJS('http://localhost:8080/silverorder/ws-stomp'); // Socket을 바로 여기서 선언
+    const stompClient = Stomp.over(socket); // WebSocket 객체를 Stomp에 넘김
 
     stompClient.connect({}, (frame) => {
       console.log('Connected to WebSocket:', frame);
-      // 특정 storeId에 대한 구독 설정
+
+      // storeId에 해당하는 특정 topic을 구독
       stompClient.subscribe(`/topic/orderIn/${storeId}`, (message) => {
-        console.log('Received message:', message.body);
-        get().onMessageReceived(); // 메시지 수신 후 처리
+        const onMessageReceived = get().onMessageReceived;
+        if (onMessageReceived) {
+          onMessageReceived(); // 메시지 수신 후 처리
+        }
       });
     }, (error) => {
       console.error('WebSocket connection error:', error);
@@ -50,11 +44,8 @@ const useWebSocketStore = create((set, get) => ({
     }
   },
 
-  // 메시지 수신 시 실행되는 함수
-  onMessageReceived: () => {
-    const { fetchOrders } = useOrderStore.getState(); // 주문 목록 갱신 함수
-    fetchOrders(); // 주문 목록 갱신
-  },
+  // 메시지 수신 시 실행되는 함수 설정
+  setOnMessageReceived: (callback) => set({ onMessageReceived: callback }),
 }));
 
 export default useWebSocketStore;
