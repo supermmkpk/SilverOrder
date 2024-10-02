901fbf112d60a2999b1a82ba74822d40d05b01a1
박동민 <pmin1616@naver.com>
2024-10-02 11:09:56 +0900

    Feat: Websocket url 동적으로 설정 테스트
    Former-commit-id: 2a199f2b5383d9f068b3d06f739e1a146db17dbc

--- Frontend/Customer_app/silverorder/src/stores/websocket.js
+++ Frontend/Customer_app/silverorder/src/stores/websocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9d9b1d6574bea6bc44dbcca45ef75f642d1616e3bd798ab177d62abf2cf421e
-size 3392
+oid sha256:8613b50f2115d07e2080c9312ca06b461c62c07df5cf34ae3f5d41b86d1472e2
+size 3374
