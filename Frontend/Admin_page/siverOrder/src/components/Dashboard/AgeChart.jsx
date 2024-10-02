f8c0674af60607f04dbb8af7503a96745327b82f
CheonYoSeong <gwpeter1000@naver.com>
2024-10-02 17:44:34 +0900

    Add : 관리자 대시보드 연령별 인기상품 차트  #479447a8
    Former-commit-id: 33153a381ea8315af90abdea65bccd0fffc58fc0

--- Frontend/Admin_page/siverOrder/src/components/Dashboard/AgeChart.jsx
+++ Frontend/Admin_page/siverOrder/src/components/Dashboard/AgeChart.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a7f2583b16692623017d48d60cb0644f2a8484586aa0f4cedeec54c2261e9c7
-size 2692
+oid sha256:e358f9e7417dbd84d6aa0551dbe31a62bb47a49f529592f06af8763a76e8c327
+size 2568
--- Frontend/Admin_page/siverOrder/src/stores/dashboard.js
+++ Frontend/Admin_page/siverOrder/src/stores/dashboard.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d220f8241da1dbb1d7d5a2bc3bae381419e875c8b855da03d479271f4c8501e0
-size 1241
+oid sha256:dbb852b639ea6bc9f4ddaf6c522efa3bae2debc8d0e1b3d6826abc06294c09c6
+size 2379
