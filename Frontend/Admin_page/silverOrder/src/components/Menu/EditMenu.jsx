872f8a2e73974f3fe75f938036e4f1d5344833a6
CheonYoSeong <gwpeter1000@naver.com>
2024-10-04 11:11:18 +0900

    Feat : 프로젝트 명이랑 기본 CSS 수정 #cc0ecf17
    Former-commit-id: 6f7ed3b0e962fc947d328ac5a091a097a3099b25

---
+++ Frontend/Admin_page/silverOrder/src/components/Menu/EditMenu.jsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:379cb67e4d88dd7339404405fddc36cdda43ae6862ff92956c967f3af33cafbb
+size 11187
---
+++ Frontend/Admin_page/silverOrder/src/stores/order.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:47fd8faee18129201510fc240346ad7fe3c5843741362c830e0f8b7527153801
+size 1966
