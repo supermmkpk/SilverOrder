d4910fc5bf71771ffc115c0f762d9b4690dc48cc
CheonYoSeong <gwpeter1000@naver.com>
2024-10-04 12:03:50 +0900

    Feat : 웹소켓 베포 환경으로 변경 #78c72d97
    Former-commit-id: 6c74af7e90c4aa9fc6c107524f5634445fcb370b

--- Frontend/Admin_page/silverOrder/src/stores/websocket.js
+++ Frontend/Admin_page/silverOrder/src/stores/websocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:041d6244032c23c539998dd318cb8a0cc5f1b813d1074c6fec4194e818b57e67
-size 2227
+oid sha256:2be9fe8a5dfa024de81a96b8876e63dea9178bfef92175e99a17dc7112e1a545
+size 2200
