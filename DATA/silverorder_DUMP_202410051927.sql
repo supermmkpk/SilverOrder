817d9a5fa2238dfccb87131cbd8936ee77693aa9
Bongkyoon <supermmkpk@gmail.com>
2024-10-05 23:34:58 +0900

    Fix: DB DUMP 외래키 제약조건 오류 해결
    Former-commit-id: deaef4d4c6ee7feaaf196a5698d908ec0a8f73b7

--- DATA/silverorder_DUMP_202410051927.sql
+++ DATA/silverorder_DUMP_202410051927.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baed4fdbdd25b2b1e70f8c9ce7aa56dc982bd20288c44465d70a869d5c89ac3e
-size 5135
+oid sha256:88bb64205d50f08c0841946e3b258c983140c6ef8046eb758a8101889ff1f471
+size 5200
