e2f0ba86fc6151d29be161658c721db91d141f05
박동민 <pmin1616@naver.com>
2024-10-07 11:35:33 +0900

    Feat: 현재 위치에 따라 Navbar의 아이콘 색상이 채워지면서 현재 페이지 알 수 있게 하는 기능 추가 #S11P21C202-47
    Former-commit-id: 536ba2005b1eb7c5b70934149c357ca4ce4313b6

--- Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
+++ Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebc23507567064e674d2d456fa36ee4fa9da108c9939f987f4be2d3a15d0af54
-size 1874
+oid sha256:eb22f3791869f9504b75ef685d1b2d4607af2676bdbdb6158620d898a5aa83f0
+size 2028
