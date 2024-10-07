57aab066e067ea40f108065548e6337d69c376f3
박봉균 <supermmkpk@gmail.com>
2024-10-07 10:57:43 +0900

    Feat: 메뉴 상태 변경 API 완성
    Former-commit-id: 7f44c6bae0a6082dc15fdfb4930c4bfe232898b6

--- Backend/src/main/java/com/silverorder/domain/menu/repository/MenuRepositoryImpl.java
+++ Backend/src/main/java/com/silverorder/domain/menu/repository/MenuRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7b446b07bcbea25d59a8772f5e4f0a40e3ac5b3e80e691fc5b006cc7c2d7c6a
-size 8815
+oid sha256:a1dcd42b4790a6c818a8d3cb379942320451de44aea674567b65e2f12500a1e4
+size 9509
---
+++ DATA/silverorder_StoredProcedure.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a1c519182838d4dc8763a0a930b5bfc9facd60dcfd05c01b95500a92c8fe258f
+size 4866
