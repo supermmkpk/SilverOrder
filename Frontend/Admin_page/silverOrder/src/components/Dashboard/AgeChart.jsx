3748feebc97a4a0bfd758fa5b325844bdc2b66e0
CheonYoSeong <gwpeter1000@naver.com>
2024-10-07 15:20:48 +0900

    Feat : 연령별 데이터 수정

--- Frontend/Admin_page/silverOrder/src/components/Dashboard/AgeChart.jsx
+++ Frontend/Admin_page/silverOrder/src/components/Dashboard/AgeChart.jsx
@@ -3,6 +3,8 @@ import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
 import './AgeChart.css';
 import useDashboardStore from '../../stores/dashboard'; // 연령별 판매 데이터를 가져오는 스토어
+import useInfoStore from '../../stores/infos';
+
 
 // Chart.js에 사용할 모듈 등록
 ChartJS.register(
@@ -16,10 +18,11 @@ ChartJS.register(
 const AgeChart = ({ title }) => {
   const [selectedAgeGroup, setSelectedAgeGroup] = useState(20); // 기본 연령대는 '20대'
   const { fetchPopularityByOrder, popularityData } = useDashboardStore(); // 상태와 함수 가져오기
+  const { userInfo } = useInfoStore();
 
   // 연령대가 변경될 때마다 API를 통해 데이터를 가져옴
   useEffect(() => {
-    fetchPopularityByOrder(1799, selectedAgeGroup); // 1799는 storeId 예시, 실제 storeId로 대체
+    fetchPopularityByOrder(userInfo.storeId, selectedAgeGroup); // 1799는 storeId 예시, 실제 storeId로 대체
   }, [selectedAgeGroup, fetchPopularityByOrder]);
 
   // popularityData가 존재하면, labels와 data를 생성
