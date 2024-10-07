0b8b3af416995c7ba9fb79b62f4126d49cd9ec8d
박동민 <pmin1616@naver.com>
2024-10-07 13:40:51 +0900

    Fix: socket 로컬용에서 배포용으로 수정
    Former-commit-id: 570477ef25c9cb4f15a7bfb185120905034beee3

--- Frontend/Customer_app/silverorder/src/stores/websocket.js
+++ Frontend/Customer_app/silverorder/src/stores/websocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa64da0d08601c566c8808a4883ec805233c7814817de75f3c15d32d8a66bad6
-size 4091
+oid sha256:ea82df7c5cd15966a48535d77b3ee82757ffbe6227d523585e36a399165af311
+size 4067
