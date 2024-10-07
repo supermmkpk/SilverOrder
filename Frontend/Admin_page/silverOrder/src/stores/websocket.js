455bf0adfe5e343c6120f8c6717807414b293216
CheonYoSeong <gwpeter1000@naver.com>
2024-10-07 14:02:19 +0900

    Feat : 소켓 변수명 설정
    Former-commit-id: 5d164a44567b51aa6d67fde7a56f610d1be403a3

--- Frontend/Admin_page/silverOrder/src/stores/websocket.js
+++ Frontend/Admin_page/silverOrder/src/stores/websocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6321462cd2edae7af899bd8f0940fe2c8d4522064e32a22cc0c764fe32288221
-size 2243
+oid sha256:75f5e2d4ca3eb5195711e1344de6b7d889288b743f2a5daa208c79ec20334c9c
+size 2248
