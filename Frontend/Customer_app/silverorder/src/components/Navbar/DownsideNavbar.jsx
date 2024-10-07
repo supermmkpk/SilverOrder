acc3e01c357ae83c7ccc83cc300a1f99921d2406
박동민 <pmin1616@naver.com>
2024-10-07 11:35:33 +0900

    Feat: 현재 위치에 따라 Navbar의 아이콘 색상이 채워지면서 현재 페이지 알 수 있게 하는 기능 추가 #S11P21C202-47

--- Frontend/Customer_app/silverorder/src/components/Navbar/DownsideNavbar.jsx
+++ Frontend/Customer_app/silverorder/src/components/Navbar/DownsideNavbar.jsx
@@ -1,13 +1,18 @@
 import "./styles/DownsideNavbar.css";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import shopping_cart from "../../img/shopping-cart-2-line.png";
+import shopping_cart_fill from "../../img/shopping-cart-2-fill.png";
 import scroll from "../../img/file-list-3-line.png";
+import scroll_fill from "../../img/file-list-3-fill.png";
 import home from "../../img/home-2-line.png";
+import home_fill from "../../img/home-2-fill.png";
 import user from "../../img/user-line.png";
+import user_fill from "../../img/user-fill.png";
 import { baseURL } from "../../constant";
 
 const DownsideNavbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const go_to_shoppingcart = () => {
     navigate(`${baseURL}/shoppingcart`);
@@ -30,7 +35,11 @@ const DownsideNavbar = () => {
       <div>
         <img
           className="downnav-btn"
-          src={shopping_cart}
+          src={
+            location.pathname.includes("/shoppingcart")
+              ? shopping_cart_fill
+              : shopping_cart
+          }
           alt="장바구니 페이지"
           onClick={go_to_shoppingcart}
         />
@@ -38,7 +47,7 @@ const DownsideNavbar = () => {
       <div>
         <img
           className="downnav-btn"
-          src={scroll}
+          src={location.pathname.includes("/orderlist") ? scroll_fill : scroll}
           alt="지난 주문 내역 확인"
           onClick={go_to_orderlist}
         />
@@ -46,7 +55,7 @@ const DownsideNavbar = () => {
       <div>
         <img
           className="downnav-btn"
-          src={home}
+          src={location.pathname.includes("/store") ? home_fill : home}
           alt="처음 화면"
           onClick={go_store_main}
         />
@@ -54,7 +63,7 @@ const DownsideNavbar = () => {
       <div>
         <img
           className="downnav-btn"
-          src={user}
+          src={location.pathname.includes("/mypage") ? user_fill : user}
           alt="마이 페이지"
           onClick={go_to_mypage}
         />
