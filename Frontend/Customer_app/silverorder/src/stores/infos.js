2debdb03ee90f522e0e656ed21fc6086f9e72ad2
박동민 <pmin1616@naver.com>
2024-10-08 15:44:25 +0900

    Feat: 매장 이름 출력, gps 아이콘 변경 #S11P21C202-120
    Former-commit-id: 8c039cd0467aa5b469d09b72cde03f6a7d7705cf

--- Frontend/Customer_app/silverorder/src/stores/infos.js
+++ Frontend/Customer_app/silverorder/src/stores/infos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7587eed4f5666f4a01351e724535391eec07a38d4fb4b977b035634928852227
-size 3460
+oid sha256:fa103d36c64bb4da910f6db9e4d24efe8532dc434c0b540f57a4db9bb7331dd5
+size 3855
--- Frontend/Customer_app/silverorder/src/stores/map.js
+++ Frontend/Customer_app/silverorder/src/stores/map.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a004f22af5f668751e427301d6265581e3ab8c878c429c2dd3cbeb566a5dcea1
+oid sha256:b7eac8b9b3b744bdf4c5612f71d2f6df7ad5786df2875b7f04b0588b58f21deb
 size 4524
