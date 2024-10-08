8ae42afb86e473f701d46d5253d93fac9a3ed57b
CheonYoSeong <gwpeter1000@naver.com>
2024-10-08 10:42:17 +0900

    Feat : 연령별 데이터 추가
    Former-commit-id: e6cfd6bee9e48c6e5e20555a39ff269941d45a1c

--- Frontend/Admin_page/silverOrder/src/components/Dashboard/AgeChart.jsx
+++ Frontend/Admin_page/silverOrder/src/components/Dashboard/AgeChart.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c7e9bf1181fd352535eb88b1b19fea373339ad29a1d28af5687ecf72077b2835
-size 2805
+oid sha256:d6d36c84d9ee85862a01c0eb371ed97e6d8c16d2bb48144d1bf06f9a2c32f28c
+size 2865
