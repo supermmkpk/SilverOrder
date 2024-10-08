56dac0d8f0565d1859dc039c1ee200fbd416cfef
Bongkyoon <supermmkpk@gmail.com>
2024-10-08 21:47:14 +0900

    Feat: 최대 할인율 즉시 결제 기능 완성
    Former-commit-id: 4834b2a791b300203e0959e8b8c698af595dc184

--- Frontend/Customer_app/silverorder/src/stores/websocket.js
+++ Frontend/Customer_app/silverorder/src/stores/websocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84a383a150e95661b45b637e39a559626cba5b563444a5cf997bf990d1f7a2b0
-size 4094
+oid sha256:0a6dfa64ad36421d59bf878deb278b449daa0c6aa595eb14c51ab856e195a1dc
+size 4457
