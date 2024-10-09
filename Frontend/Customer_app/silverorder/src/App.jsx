10d0901a1bb0963a3bc4bb6ac482b9a16aee8ca0
Bongkyoon <supermmkpk@gmail.com>
2024-10-09 23:49:13 +0900

    Feat, Build: 추천 결제 confirm 창 수정 및 MUI 적용
    Former-commit-id: c9173a0a55c57bbeb56ab72dcc4561acf6fc5fc8

--- Frontend/Customer_app/silverorder/src/App.jsx
+++ Frontend/Customer_app/silverorder/src/App.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66f61a5ee354294faaf82df6febfb6b5ad1b4425ef530d98734e516fc49a6476
-size 1799
+oid sha256:7921823ebb1451c24b7a593fd8b4b5dcae23a0c2c5055c8607adebead7fb4bd6
+size 1800
