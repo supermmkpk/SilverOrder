c1d52108fddff79c42320c542bfacdcf38579da6
Bongkyoon <supermmkpk@gmail.com>
2024-10-10 02:32:45 +0900

    Refactor: 추천결제 Confirm 버튼 수정
    Former-commit-id: 97d777da405fbac07849dc1378ed60c5c3d3895b

--- Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
+++ Frontend/Customer_app/silverorder/src/views/PurchasePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b48107cfea316b12b6aefba6ceb576fc06c2b67c0159304b6144a19a4fa70fc
-size 8359
+oid sha256:44d55560b71501310fd1068953dd54a9c93444f9d66e84f5c7d7b396a5919cc8
+size 8370
