577738e63c3fc0280e29112d9ce329bf6249d583
Bongkyoon <supermmkpk@gmail.com>
2024-10-10 02:23:00 +0900

    Feat: QR 코드 스캔 기능 추가
    Former-commit-id: 05cd58819a2c47f9cb2873e3b4112cdf77102473

--- Frontend/Customer_app/silverorder/src/views/OutdoorPage.jsx
+++ Frontend/Customer_app/silverorder/src/views/OutdoorPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df55885dd38f1062521f1af944d35f607245f3be61b822e0ab75b952abe7e7f7
-size 800
+oid sha256:9e9c28b18a7239b7e4d945a415d7ec342304d2e837a49e927c97a91af69c93d2
+size 3617
