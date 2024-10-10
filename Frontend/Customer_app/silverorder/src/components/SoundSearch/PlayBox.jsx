28d9c5f8c9354296f8ba58401c1b3b4c4fbb8f57
박동민 <pmin1616@naver.com>
2024-10-10 12:11:32 +0900

    Feat: 녹음 종료 버튼을 누르면 마이크도 꺼지도록 수정 #S11P21C202-19
    Former-commit-id: 9bf42dd396e8cab900411a06deb4b61e31e3dba3

--- Frontend/Customer_app/silverorder/src/components/SoundSearch/PlayBox.jsx
+++ Frontend/Customer_app/silverorder/src/components/SoundSearch/PlayBox.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4036f4ad0782fb20f972c1112e9240cb7bcff4a9f7c38fd58335a8fce2169952
-size 9314
+oid sha256:19f8d9469239e11c0f8270c6108839809a2abcf676f1ad412f1b0ce97cf00910
+size 9452
