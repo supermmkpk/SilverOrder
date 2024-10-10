39d404cad6ffbd7e3617783cd0d3493506aca6b4
박봉균 <supermmkpk@gmail.com>
2024-10-10 11:23:01 +0900

    Fix: 배포 TTS 시도 #2
    Former-commit-id: c55c0428b9fc5ff217877d81e3a4d26f8a7fc73d

--- Backend/src/main/java/com/silverorder/domain/voice/service/TTSServiceImpl.java
+++ Backend/src/main/java/com/silverorder/domain/voice/service/TTSServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1818a7402fa74e386433f180a6fd5aa87f0da39eb2d2d98eabef50b377f7535c
-size 1651
+oid sha256:67d4c80097aeb63a8832a8598c0f9bab832f15aaca2563fe7235788c20eaf282
+size 1904
