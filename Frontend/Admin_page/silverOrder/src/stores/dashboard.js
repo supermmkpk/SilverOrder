d1b903c734349100e60f2c7d646dc80aec3c47e2
CheonYoSeong <gwpeter1000@naver.com>
2024-10-10 10:26:52 +0900

    Feat : 알림 설정 #53467b68
    Former-commit-id: 9a006b11102f0c3eb94329bc991cadb49bea77ec

--- Frontend/Admin_page/silverOrder/src/stores/dashboard.js
+++ Frontend/Admin_page/silverOrder/src/stores/dashboard.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbb852b639ea6bc9f4ddaf6c522efa3bae2debc8d0e1b3d6826abc06294c09c6
-size 2379
+oid sha256:b6b43266cc066a9d02da95e9408aa13c94108e1098f8b7758085623720f11aa7
+size 2382
