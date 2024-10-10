5ea815278d68658dc718a0b1f41714bdda9c0d14
박봉균 <supermmkpk@gmail.com>
2024-10-10 11:07:49 +0900

    Fix: 배포환경에서 TTS mp3파일 경로 수정
    Former-commit-id: a867036fe3ab50e2654ac27b555d61fcdb1f43e0

--- Backend/src/main/java/com/silverorder/domain/voice/service/TTSServiceImpl.java
+++ Backend/src/main/java/com/silverorder/domain/voice/service/TTSServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:643655848f06f60279ce778458ccca610da47fe9c4eaa511b5b508e6ffad71a0
-size 1643
+oid sha256:1818a7402fa74e386433f180a6fd5aa87f0da39eb2d2d98eabef50b377f7535c
+size 1651
