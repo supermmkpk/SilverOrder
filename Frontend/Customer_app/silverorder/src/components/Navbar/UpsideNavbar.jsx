d6cff7073a5563498552d0e62294f2a450a93792
박동민 <pmin1616@naver.com>
2024-10-10 10:20:32 +0900

    Design, Feat: css 수정, 미확인 알림 있을 시 시계 아이콘 깜빡이는 기능 추가 #S11P21C202-120
    Former-commit-id: 5c3958940422f90a0197fb648afb7027102a14dc

--- Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
+++ Frontend/Customer_app/silverorder/src/components/Navbar/UpsideNavbar.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb22f3791869f9504b75ef685d1b2d4607af2676bdbdb6158620d898a5aa83f0
-size 2028
+oid sha256:e8983fe430e2f0a98869dadc99875a9292c36b60322ea2257b5889fc73fb44b4
+size 2400
